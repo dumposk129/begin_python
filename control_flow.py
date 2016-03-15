6dd66cd52b9e5d4a56f9f14345d9864a5ccbe121
dumposk129 <dumposk129@gmail.com>
2016-03-15 16:25:06 +0700

    control flow

---
+++ control_flow.py
@@ -0,0 +1,49 @@
+# if Statements
+# x = int(input("Please enter an integer "))
+# print(x)
+# if x < 0:
+#     x = 0
+#     print('Negative changed to zero')
+# elif x == 0:
+#     print('Zero')
+# elif x == 1:
+#     print('Single')
+# else:
+#     print('More')
+
+# for Statements
+# words = ['cat', 'windows', 'defenestrate']
+# for w in words:
+#     print(w, len(w))
+
+# break and continue Statements and else Clauses on Loops
+# for n in range(2, 10):
+#     for x in range(2, n):
+#         if n % x == 0:
+#             print(n, 'equals', x, '*', n//x)
+#             break
+#         else:
+#             # loop fell through without finding a factor
+#             print(n, 'is a prime number')
+
+# Defining Function
+# def fib(n):
+#     a, b = 0, 1
+#     while a < n:
+#         print(a, end=' ')
+#         a, b = b, a + b
+#
+# print(fib(2000))
+#
+# # List
+# print(list(range(3, 6)))
+
+# Lambda Expression
+# def make_incrementor(n):
+#     return lambda x: x + n
+# f = make_incrementor(42)
+# print(f(0))
+
+pairs = [(1, 'one'), (2, 'two'), (3, 'three'), (4, 'four')]
+pairs.sort(key = lambda pair: pair[1])
+print(pairs)
--- data_structures.py
+++ data_structures.py
@@ -145,12 +145,12 @@
 # for i in sorted(set(basket)):
 #     print(i)
 
-import math
-raw_data = [56.2, float('NaN'), 51.7, 55.3, 52.5,
-            float('NaN'), 47.8]
-filtered_data = []
-for value in raw_data:
-    if not math.isnan(value):
-        filtered_data.append(value)
-
-print(filtered_data)
+# import math
+# raw_data = [56.2, float('NaN'), 51.7, 55.3, 52.5,
+#             float('NaN'), 47.8]
+# filtered_data = []
+# for value in raw_data:
+#     if not math.isnan(value):
+#         filtered_data.append(value)
+#
+# print(filtered_data)
