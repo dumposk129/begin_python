8d9b7bafd0a1eadfc9953817c5a1d58b6c70984b
dumposk129 <dumposk129@gmail.com>
2016-03-17 10:14:14 +0700

    input_output

--- input_output.py
+++ input_output.py
@@ -25,10 +25,10 @@ import json
 # io = StringIO('["streaming API"]')
 # print(json.load(io))
 
-def as_complex(dct):
-    if '__complex__' in dct:
-        return complex(dct['real'], dct['imag'])
-    return dct
-
-json.loads('{"__complex__": true, "real": 1, "imag": 2}',
-           object_hook=as_complex)
+# def as_complex(dct):
+#     if '__complex__' in dct:
+#         return complex(dct['real'], dct['imag'])
+#     return dct
+#
+# print(json.loads('{"__complex__": true, "real": 1, "imag": 2}',
+#            object_hook=as_complex))
